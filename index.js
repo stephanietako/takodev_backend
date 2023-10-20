e172259ece06c5423aef354feff930fe7741e17e
stephanietako <gabeshine@live.fr>
2023-10-20 05:35:32 +0200

    wip: connection from node js to brevo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import fetch from "node-fetch";
 import { validateForm } from "./validator.js";
 import { createTransport } from "nodemailer";
 import helmet from "helmet";
@@ -10,45 +11,89 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("welcome to the tako dev backend follow the white rabbit");
+  res.send("Welcome to the Tako Dev backend. Follow the white rabbit.");
 });
 
-app.post("/contact", async (req, res) => {
-  const { error, value } = validateForm(req.body);
+// Ici c'est pour l'envoi de mail aux utilisateurs
+app.post("/send-mail", async (req, res) => {
+  const { error, value } = req.body.mail;
   if (error) {
     console.log(error.details);
-    return res
-      .status(400)
-      .send("You missed the rabbit hole, the data sent are invalid");
+    return res.status(400).json({ error: "Invalid data sent send email." });
   }
-
+  res.json({ success: true });
   const transporter = createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     auth: {
       user: process.env.TRANSP_USER,
-      pass: process.env.API_KEY,
+      pass: process.env.SMTP_KEY,
     },
   });
 
-  const mailOptions = {
-    from: process.env.TRANSP_USER,
-    to: req.body.email,
-    subject: "Vous avez des questions ? Un projet ? ",
-    text: "contactez-moi pour en discuter",
+  transporter.verify(async (err, success) => {
+    if (err) {
+      console.error(err);
+      return res.json({ success: false });
+    }
+
+    const mailOptions = {
+      from: process.env.TRANSP_USER,
+      to: req.body.email,
+      subject: "Contact Response from Tako Dev",
+      text: "Thank you for contacting Tako Dev! We hope to answer all your questions.",
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.error(error, "erreur dans le transporteur de sendmail");
+      return res.json({ success: false });
+    }
+  });
+});
+
+// Ici c'est pour l'envoi de sinput du formulaire sur Brevo
+const apiKey = process.env.API_KEY;
+app.post("/contact", async (req, res) => {
+  console.log("req.body", req.body);
+  const { error, value } = validateForm(req.body);
+  console.log("req.body.name", req.body.name);
+  if (error) {
+    console.log(error.details);
+    return res.status(400).json({ error: "Invalid data sent contact form." });
+  }
+
+  // const { name, lastname, email, message } = req.body;
+  const contact = {
+    name: req.body.name,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    message: req.body.message,
   };
 
   try {
-    await transporter.sendMail(mailOptions);
-    res.send("Succefully you have reached the rabbit hole");
+    const response = await fetch("https://api.brevo.com/v3/contacts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Api-Key": apiKey,
+      },
+      body: JSON.stringify(contact),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
-    console.log(error);
-    res.status(500).send("Something went wrong, you missed the rabbit hole ");
+    console.error(error);
   }
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Application démarée sur le port : ${process.env.PORT}`)
+  console.log(`Application started on port: ${process.env.PORT}`)
 );
--- validator.js
+++ validator.js
@@ -1,18 +1,7 @@
 import Joi from "joi";
-import * as sanitizeHtml from "sanitize-html";
 
-const validator = (shema) => (payload) => {
-  // Échappement les valeurs de chaîne si nécessaire
-  payload.name = encodeURIComponent(payload.name);
-  payload.lastname = encodeURIComponent(payload.lastname);
-  payload.message = encodeURIComponent(payload.message);
-  // Nettoyer le HTML des données du formulaire
-  payload.name = sanitizeHtml(payload.name).cleaned;
-  payload.lastname = sanitizeHtml(payload.lastname).cleaned;
-  payload.message = sanitizeHtml(payload.message).cleaned;
-
-  return shema.validate(payload, { abortEarly: false });
-};
+const validator = (shema) => (payload) =>
+  shema.validate(payload, { abortEarly: false });
 
 const formSchema = Joi.object({
   name: Joi.string().alphanum().min(3).max(30).required(),
