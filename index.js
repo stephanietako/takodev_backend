823efec46c1f140ed28cf123a8bf50586f8f838c
stephanietako <gabeshine@live.fr>
2023-10-23 19:09:28 +0200

    wip: formulaire contact & email

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import fetch from "node-fetch";
 import { validateForm } from "./validator.js";
 import helmet from "helmet";
 import SibApiV3Sdk from "sib-api-v3-sdk";
+import send from "./mailing.js";
 
 const app = express();
 
@@ -12,8 +13,61 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  res.send("Welcome to the Tako Dev backend. Follow the white rabbit.");
+
+// Fonction pour configurer l'API Brevo
+const setUpBrevo = () => {
+  let defaultClient = SibApiV3Sdk.ApiClient.instance;
+  let apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey = process.env.API_KEY;
+};
+
+// Fonction pour stocker des datas de contact d'utilisateur dans Brevo
+const sendEmailviaBrevo = async (toEmail, listId, res) => {
+  setUpBrevo();
+
+  const apiInstance = new SibApiV3Sdk.ContactsApi();
+  const createContact = {
+    email: toEmail,
+    listIds: [listId],
+    emailBlacklisted: false,
+    smsBlacklisted: false,
+    updateEnabled: false,
+  };
+  console.log(
+    "Paramètres pour l'appel à l'API : " + JSON.stringify(createContact)
+  );
+  try {
+    await apiInstance.createContact(createContact);
+    console.log("E-mail envoyé avec succès via Brevo");
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'e-mail avec Brevo : " + error);
+    console.error(error);
+    res.json({ success: false });
+  }
+};
+
+app.post("/email", async (req, res) => {
+  console.log("Requête POST /email reçue.");
+
+  const { error, value } = req.body.email;
+  if (error) {
+    console.log(error.details);
+    return res.status(400).json({ error: "Invalid data sent send email." });
+  }
+
+  try {
+    const emailSent = send(req.body.email);
+
+    if (emailSent) {
+      sendEmailviaBrevo(req.body.email, 4, res);
+      return res.status(200).json({ success: true });
+    } else {
+      return res.status(403).json({ error: "Failed to send email." });
+    }
+  } catch (error) {
+    return res.status(403).json({ error: error.message });
+  }
 });
 
 app.post("/contact", async (req, res) => {
@@ -22,59 +76,36 @@ app.post("/contact", async (req, res) => {
     console.log(error.details);
     return res.status(400).json({ error: "Invalid data sent ." });
   }
-  // Récupérez les données du formulaire
+
   const { firstname, lastname, email, message } = req.body;
 
-  let defaultClient = SibApiV3Sdk.ApiClient.instance;
-  let apiKey = defaultClient.authentications["api-key"];
-  apiKey.apiKey = process.env.API_KEY;
+  apiBrevo();
 
   const apiInstance = new SibApiV3Sdk.ContactsApi();
-
-  let createContact = new SibApiV3Sdk.CreateContact();
-  (createContact = {
-    email: email,
+  const createContact = {
+    email,
     attributes: {
       MESSAGE: message,
       PRENOM: firstname,
       NOM: lastname,
     },
-
     listIds: [3],
     emailBlacklisted: false,
     smsBlacklisted: false,
     updateEnabled: false,
-  }),
-    apiInstance.createContact(createContact).then(
-      function (data) {
-        console.log("API called successfully. Returned data: ", data);
-        res.json({ success: true });
-      },
-      function (error) {
-        console.error(error);
-        res.json({ success: false });
-      }
-    );
+  };
 
   try {
-    const response = await fetch("https://api.brevo.com/v3/contacts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Api-Key": apiKey,
-      },
-      body: JSON.stringify(createContact),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Request failed with status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log(data);
+    await apiInstance.createContact(createContact);
+    res.json({ success: true });
   } catch (error) {
     console.error(error);
+    res.json({ success: false });
   }
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Tako Dev backend. Follow the white rabbit.");
 });
 
 app.listen(process.env.PORT, () =>
--- mailing.js
+++ mailing.js
@@ -1,28 +1,8 @@
-import express from "express";
-import cors from "cors";
 import "dotenv/config";
-import fetch from "node-fetch";
-import { validateForm } from "./validator.js";
 import { createTransport } from "nodemailer";
-import helmet from "helmet";
 
-const app = express();
-
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Welcome to the Tako Dev backend. Follow the white rabbit.");
-});
-
-// Ici c'est pour l'envoi de mail aux utilisateurs
-app.post("/send-mail", async (req, res) => {
-  const { error, value } = req.body.mail;
-  if (error) {
-    console.log(error.details);
-    return res.status(400).json({ error: "Invalid data sent send email." });
-  }
-  res.json({ success: true });
+const send = (toEmail) => {
+  console.log("Envoi d'email à l'adresse : " + toEmail);
   const transporter = createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
@@ -32,24 +12,20 @@ app.post("/send-mail", async (req, res) => {
     },
   });
 
-  transporter.verify(async (err, success) => {
-    if (err) {
-      console.error(err);
-      return res.json({ success: false });
-    }
-
-    const mailOptions = {
-      from: process.env.TRANSP_USER,
-      to: req.body.email,
-      subject: "Contact Response from Tako Dev",
-      text: "Thank you for contacting Tako Dev! We hope to answer all your questions.",
-    };
-
-    try {
-      await transporter.sendMail(mailOptions);
-    } catch (error) {
-      console.error(error, "erreur dans le transporteur de sendmail");
-      return res.json({ success: false });
+  const mailOptions = {
+    from: process.env.TRANSP_USER,
+    to: toEmail,
+    subject: "Contact Tako Dev",
+    text: "Merci d'avoir contacter Tako Dev ",
+  };
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("E-mail envoyé : " + info.response);
     }
   });
-});
+};
+
+export default send;
